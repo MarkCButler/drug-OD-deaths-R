7ce831715e5c9e05806a4250d4d6359fcabe263b
Mark Butler <mrkcbutler@gmail.com>
2020-05-07 07:22:14 -0500

    Reorganize file structure

--- server.R
+++ server.R
@@ -2,47 +2,7 @@ library(DT)
 library(shiny)
 library(googleVis)
 
-source('./db_helper.R')
-
-# The population.csv file was created by first manually modifying the .xlsx file
-# downloaded from www.census.gov to create a csv file and then doing
-# preprocessing with the script preprocess.R written for this app.  The csv file
-# contains population estimates for the US and each of the 50 states.  For each
-# of the years 2014 - 2019, there is an estimate of the population on July 1.
-population <- read.csv('./data/population.csv', row.names = 1)
-
-# In the data on deaths by drug OD, dates are given in the format 'January 2015'.
-# Define a function for converting this format to a Date object.
-convert.to.Date <- function(month.year) {
-    date.object <-  as.Date(paste('01', month.year),
-                           format = '%d %B %Y')
-    return(date.object)
-}
-
-# Define a function for finding interpolated populations.  
-#
-# First lapply is used to define one function for each row of the population
-# data frame.  Then a single function is defined to do the interpolation given a
-# region and a vector Date objects.
-#
-# Note that this interpolation is needed primarily for time-series plots that
-# show number of deaths per 100,000 population.  The interpolation avoids
-# spurious jumps in such plots.
-population.dates = convert.to.Date(
-    paste('July', seq(from = 2014, to = 2019))
-)
-get.interp.function <- function(region) {
-   approxfun(x = population.dates,
-             y = as.numeric(population[region,]),
-             method = 'linear',
-             rule = 2) 
-}
-interp.functions <- lapply(rownames(population), get.interp.function)
-names(interp.functions) <- rownames(population)
-get.population <- function(region, dates) {
-    population.estimates <- interp.functions[[region]](dates)
-    return(population.estimates)
-}
-
+source('./database.R')
+source('./time_series.R')
 
 server <- function(input, output, session) {}
